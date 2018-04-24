31d1e79355f01577e4260fc202fc9b6c53e7437c
Nguyen Van Bong <van.bong@grabtaxi.com>
2018-04-24 13:15:36 +0800

    Add tslint and fix errors

--- lib/main.ts
+++ lib/main.ts
@@ -1,98 +1,138 @@
-import { CompositeDisposable, TextEditor } from 'atom'
-import { spawnSync } from 'child_process'
+import { CompositeDisposable, Range, TextEditor } from "atom";
+import { IndieDelegate, Message} from "atom/linter";
+import { spawnSync } from "child_process";
 
-interface FormatTool {
-  name: string
-  cmd: string
-  args: Array<string>
-  onSave: boolean
+interface IFormatTool {
+  name: string;
+  cmd: string;
+  args: string[];
+  onSave: boolean;
 }
 
 class Formatter {
-  subscriptions: CompositeDisposable
-  commandsSubscriptions: CompositeDisposable | null
-  config: any
+  public config: any;
+
+  private subscriptions: CompositeDisposable;
+  private linter: IndieDelegate | null;
 
   constructor() {
-    this.subscriptions = new CompositeDisposable()
-    this.commandsSubscriptions = null
-    this.config = require('./config-schema.json')
+    this.subscriptions = new CompositeDisposable();
+    this.config = require("./config-schema.json");
+    this.linter = null;
   }
 
-  activate () {
+  public activate() {
     // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
-    this.subscriptions = new CompositeDisposable()
-
-    this.subscriptions.add(atom.config.observe('go-formatter.formatTools', (formatTools) => {
-      this.registerFormatTools(formatTools)
-    }))
+    this.subscriptions = new CompositeDisposable();
+    this.subscriptions.add(atom.config.observe("go-formatter.formatTools", (formatTools) => {
+      this.registerFormatTools(formatTools);
+    }));
   }
 
-  deactivate () {
-    this.subscriptions.dispose()
-    if (this.commandsSubscriptions) {
-      this.commandsSubscriptions.dispose()
-    }
+  public deactivate() {
+    this.subscriptions.dispose();
   }
 
-  registerFormatTools (formatTools: Array<FormatTool>) {
-    if (this.commandsSubscriptions) {
-      this.commandsSubscriptions.dispose()
-    }
-    this.commandsSubscriptions = new CompositeDisposable()
-    for (let formatTool of formatTools) {
-      this.commandsSubscriptions.add(atom.commands.add('atom-text-editor[data-grammar~="go"]', 'golang:'+formatTool.name, () => {
-        this.format(atom.workspace.getActiveTextEditor(), formatTool)
-      }))
+  public consumeIndie(register: (opts: {name: string}) => IndieDelegate) {
+    this.linter = register({
+      name: "go-formatter",
+    });
+    this.subscriptions.add(this.linter);
+  }
+
+  private registerFormatTools(formatTools: IFormatTool[]) {
+    const commandsSubscriptions = new CompositeDisposable();
+    this.subscriptions.add(commandsSubscriptions);
+    for (const formatTool of formatTools) {
+      commandsSubscriptions.add(atom.commands.add(
+        "atom-text-editor[data-grammar~=\"go\"]",
+        "golang:" + formatTool.name, () => {
+          this.format(atom.workspace.getActiveTextEditor(), formatTool);
+        },
+      ));
+
       if (formatTool.onSave) {
-        this.commandsSubscriptions.add(atom.workspace.observeTextEditors((editor) => {
-          if (!editor || !editor.getBuffer() || !this.commandsSubscriptions) {
-            return
+        commandsSubscriptions.add(atom.workspace.observeTextEditors((editor) => {
+          if (!editor || !editor.getBuffer()) {
+            return;
           }
 
-          let bufferSubscriptions = new CompositeDisposable()
+          const bufferSubscriptions = new CompositeDisposable();
           bufferSubscriptions.add(editor.getBuffer().onWillSave(() => {
-            this.format(editor, formatTool)
-          }))
+            this.format(editor, formatTool);
+          }));
           bufferSubscriptions.add(editor.getBuffer().onDidDestroy(() => {
-            bufferSubscriptions.dispose()
-          }))
-          this.commandsSubscriptions.add(bufferSubscriptions)
-        }))
+            bufferSubscriptions.dispose();
+          }));
+          commandsSubscriptions.add(bufferSubscriptions);
+        }));
       }
     }
   }
 
-  format (editor: TextEditor | undefined, tool: FormatTool) {
+  private format(editor: TextEditor | undefined, tool: IFormatTool) {
     if (!editor || !this.isValidEditor(editor) || !editor.getBuffer()) {
-      return
+      return;
     }
 
-    let options = {
-      encoding: 'utf8',
+    const options = {
+      encoding: "utf8",
       env: process.env,
-      input: editor.getText()
+      input: editor.getText(),
+    };
+
+    if (this.linter) {
+      this.linter.clearMessages();
     }
 
-    let r = spawnSync(tool.cmd, tool.args, options)
+    const r = spawnSync(tool.cmd, tool.args, options);
     if (r.stderr && r.stderr.length > 0) {
-      console.log('go-formatter: (stderr) ' + r.stderr)
-      return
+      if (this.linter) {
+        this.linter.setAllMessages(this.parseErrors(editor.getPath(), r.stderr));
+      }
+      return;
     }
     if (r.status === 0 && r.stdout && r.stdout.length > 0) {
-      editor.getBuffer().setTextViaDiff(r.stdout.toString())
-      return
+      editor.getBuffer().setTextViaDiff(r.stdout.toString());
+      return;
     }
-    console.log('go-formatter: skipped ' + r.status)
   }
 
-  isValidEditor (editor: TextEditor): boolean {
+  private parseErrors(file: string | undefined, stderr: Buffer): Message[] {
+    if (!file) {
+      return [];
+    }
+    const pattern = /^<.*>:(\d+):(\d+):(.*)?$/img;
+    const messages: Message[] = [];
+    while (true) {
+      const match = pattern.exec(stderr.toString());
+      if (!match) {
+        break;
+      }
+      if (match.length < 4) {
+        continue;
+      }
+      const line = parseInt(match[1], 10);
+      const offset = parseInt(match[2], 10);
+      messages.push({
+        excerpt: match[3],
+        location: {
+          file,
+          position: new Range([line - 1, offset - 1], [line - 1, offset]),
+        },
+        severity: "error",
+      });
+    }
+    return messages;
+  }
+
+  private isValidEditor(editor: TextEditor): boolean {
     if (!editor.getGrammar()) {
-      return false
+      return false;
     }
 
-    return editor.getGrammar().scopeName === 'source.go'
+    return editor.getGrammar().scopeName === "source.go";
   }
 }
 
-module.exports = new Formatter()
+module.exports = new Formatter();
